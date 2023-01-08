72b577caaec6ef5ba8110c13f10be6e6d9f747f4
Lukasz Rybak <lukirybak@gmail.com>
2023-01-08 22:21:59 +0100

    Exercise completed

---
+++ StudentsController/Models/Student.cs
@@ -0,0 +1,39 @@
+﻿namespace StudentsController.Models
+{
+    public class Student
+    {
+        public string Fname { get; set; }
+        public string Lname { get; set; }
+        public string IndexNumber { get; set; }
+        public string Birthdate { get; set; }
+        public string Studies { get; set; }
+        public string Mode { get; set; }
+        public string Email { get; set; }
+        public string MothersName { get; set; }
+        public string FathersName { get; set; }
+
+
+        public Student(
+            string fname,
+            string lname,
+            string indexNumber,
+            string birthdate,
+            string studies,
+            string mode,
+            string email,
+            string mothersName,
+            string fathersName
+            )
+        {
+            this.Fname = fname;
+            this.Lname = lname;
+            this.IndexNumber = indexNumber;
+            this.Birthdate = birthdate;
+            this.Studies = studies;
+            this.Mode = mode;
+            this.Email = email;
+            this.MothersName = mothersName;
+            this.FathersName = fathersName;
+        }
+    }
+}
